3e61502bbda901b3295d8d5dcd28de99c87b688c
Fathurrobbany <fathurrobbany5@gmail.com>
2025-06-13 23:16:29 +0700

    update

--- config/db.js
+++ config/db.js
@@ -15,18 +15,5 @@ db.connect(err => {
     console.log('Connected to MySQL database');
 });
 
-// Buat tabel jika belum ada
-db.query(`
-    CREATE TABLE IF NOT EXISTS users (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        full_name VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL UNIQUE,
-        username VARCHAR(255) NOT NULL UNIQUE,
-        password VARCHAR(255) NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    )
-`, err => {
-    if (err) console.error('Error creating table:', err);
-});
 
 module.exports = db;
